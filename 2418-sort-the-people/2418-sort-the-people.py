baa52516a48d75b05012ddb52b9d06448556ff2e
bmaker32 <88323306+bmaker32@users.noreply.github.com>
2023-01-17 18:59:52 +0300

    Time: 513 ms (11.88%), Space: 14.4 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,4 +1,16 @@
 class Solution:
-    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:          
-        return [x for _,x in sorted(zip(heights,names),reverse=True)]
+    def sortPeople(self, names: List[str], height: List[int]) -> List[str]:          
+        
+        for i in range(len(height)):
+            mini = height[i]
+            ind = i
+            for j in range(i+1,len(height)):
+                
+                if mini < height[j]:
+                    mini = height[j]
+                    ind = j
+            height[i],height[ind] = height[ind],height[i]
+            names[i],names[ind] = names[ind],names[i]
+        return names 
+                
                 
