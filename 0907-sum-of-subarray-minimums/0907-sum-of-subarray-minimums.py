5a8f2ced6086e207c9429378ec7bedc88da06542
bmaker32 <88323306+bmaker32@users.noreply.github.com>
2023-03-21 14:13:40 +0300

    Time: 439 ms (97.10%), Space: 18.8 MB (84.75%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        
+        M = 10**9+7
+        sumMin = 0
+        arr.append(0)  
+        stack = [-1]  
+        
+        
+        for cur_idx, num in enumerate(arr):
+            while stack and num < arr[stack[-1]]:
+                idx = stack.pop()
+                pre_min = stack[-1]
+                sumMin += (idx - pre_min) * (cur_idx - idx) * arr[idx]
+            stack.append(cur_idx)
+        
+        return sumMin%M
+        
