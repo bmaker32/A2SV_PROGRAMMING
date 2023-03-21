d1ad8d9897ab176a0425267b248c62b4c12d35b8
bmaker32 <88323306+bmaker32@users.noreply.github.com>
2023-03-21 17:21:34 +0300

    added binary-tree-paths.python

---
+++ binary-tree-paths.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def helper(self,root,strin,ans):
+
+        if not root.left and not root.right:
+            ans.append(strin + str(root.val))
+            return 
+        if root.left:
+            self.helper(root.left,strin + str(root.val) + "->",ans)
+        if root.right:
+            self.helper(root.right,strin + str(root.val) + "->",ans)
+        
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        ans = []
+        
+        self.helper(root,"",ans)
+        return ans
