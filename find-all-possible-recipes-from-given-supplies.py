1d7910c1bfa68e5a9ab3b7d802f312ed92f60dc8
bmaker32 <88323306+bmaker32@users.noreply.github.com>
2023-06-01 08:52:04 +0300

    added find-all-possible-recipes-from-given-supplies.python

---
+++ find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def findAllRecipes(self, recipes, ingredients, supplies):
+        """
+        :type recipes: List[str]
+        :type ingredients: List[List[str]]
+        :type supplies: List[str]
+        :rtype: List[str]
+        """
+   
+        supplies=set(supplies)
+        graph=defaultdict(list)
+        n=len(recipes)
+        in_degree=defaultdict(int)
+        for i in range(n):
+            u=recipes[i]
+            for v in ingredients[i]:
+                graph[v].append(u)
+                in_degree[u]+=1
+        q=deque([i for i in supplies])
+        while q:
+            curr_supp=q.popleft()
+            for nei in graph[curr_supp]:
+                in_degree[nei]-=1
+                if in_degree[nei]==0: q.append(nei)
+        ans=[]
+        for recipe in recipes:
+            if in_degree[recipe]<=0: ans.append(recipe)
+        return ans
